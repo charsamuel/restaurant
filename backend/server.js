7bc25edac3ec6d040195bb84946a9911badadbbe
Charity Samuel <scharity463@gmail.com>
2024-10-14 21:16:01 +0300

    ran npm run build

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,14 @@
-import app from "./app.js";
+const express = require('express');
+const path = require('path');
+const app = express();
 
-app.listen(process.env.PORT, ()=>{
-    console.log(`SERVER HAS STARTED AT PORT ${process.env.PORT}`);
-})
+app.use(express.static(path.join(__dirname, 'client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/dist', 'index.html'));
+});
+
+const PORT = process.env.PORT || 4000
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
